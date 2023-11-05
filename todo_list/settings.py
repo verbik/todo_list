4145a8e8be532692c924e15d1c146d65032b5ab4
Yuliia <verbikyullya@gmail.com>
2023-11-05 22:44:30 +0200

    chore: - add bootrstrap to installed apps

--- todo_list/settings.py
+++ todo_list/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "crispy_bootstrap4",
+    "crispy_forms",
     "app",
 ]
 
